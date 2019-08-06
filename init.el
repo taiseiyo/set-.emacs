eece52539f17c7da5fdb699e00b4e3021e4e84eb
taiseiyo <53172326+taiseiyo@users.noreply.github.com>
2019-08-06 19:03:06 +0900

    Add files via upload

---
+++ init.el
@@ -0,0 +1,16 @@
+(autoload 'mew "mew" nil t)
+(autoload 'mew-send "mew" nil t)
+
+;;カッコの部分を明るくさせる
+(show-paren-mode t)
+(setq show-paren-style 'parenthesis) 
+
+;;追加のパッケージを登録
+(package-initialize)
+(setq package-archives
+      '(("gnu" . "http://elpa.gnu.org/packages/")
+        ("melpa" . "http://melpa.org/packages/")
+        ("org" . "http://orgmode.org/elpa/archive-contents/")))
+
+(add-hook 'python-mode-hook 'jedi:setup)
+(setq jedi:complete-on-dot t)
