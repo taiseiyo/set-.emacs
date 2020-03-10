6674c5b23f5080a56c7935ded40afc1a253ab796
taiseiyo <53172326+taiseiyo@users.noreply.github.com>
2020-03-10 19:29:24 +0900

    fix program

--- el-files/wise-compile.el
+++ el-files/wise-compile.el
@@ -4,23 +4,24 @@
   
   (setq pro_name (buffer-file-name)
 	py_name ".py"
-	sh_name ".sh")
+	sh_name ".sh"
+	el_name ".el")
   
   (if (string-equal py_name
 		    (substring pro_name -3))
       (compile (concat "python3 " pro_name))
-  (if (string-equal sh_name
+    (if (string-equal sh_name
 		      (substring pro_name -3))
 	(compile (concat "bash " pro_name))
-      (if (string-equal ".el"
-		      (substring pro_name -3))
+      (if (string-equal el_name
+			(substring pro_name -3))
 	  (byte-compile-file (file-name-nondirectory pro_name))
 	(message "Not compatible with this mode")
 	)
       )
+    )
   )
-)
-  
+
 (provide 'wise-compile)
 
 ;;wise-compile.el is end here
