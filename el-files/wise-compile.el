aaf7d1a4db9dbc5f0863c0f3cba872b9d94d9c81
taiseiyo <53172326+taiseiyo@users.noreply.github.com>
2020-03-10 18:52:45 +0900

    fix program

--- el-files/wise-compile.el
+++ el-files/wise-compile.el
@@ -1,21 +1,26 @@
 (defun wise-compile()
   "This function compile quickly for specified program"
   (interactive)
-  ;; (setq pro_name buffer-file-name)
-  (if (string-equal ".py"
-		    (substring (buffer-file-name) -3))
-      (compile (concat "python3 "
-		       (message (buffer-file-name))))
-    (if (string-equal ".sh"
-		      (substring (buffer-file-name) -3))
-	(compile (concat "bash "
-			 (message (buffer-file-name))))
+  
+  (setq pro_name (buffer-file-name)
+	py_name ".py"
+	sh_name ".sh")
+  
+  (if (string-equal py_name
+		    (substring pro_name -3))
+      (compile (concat "python3 " pro_name))
+  (if (string-equal sh_name
+		      (substring pro_name -3))
+	(compile (concat "bash " pro_name))
       (if (string-equal ".el"
-		      (substring (buffer-file-name) -3))
-	  (byte-compile-file (file-name-nondirectory (buffer-file-name)))
+		      (substring (pro_name) -3))
+	  (byte-compile-file (file-name-nondirectory (pro_name)))
 	(message "Not compatible with this mode")
 	)
       )
-    )
   )
+)
+  
 (provide 'wise-compile)
+
+;;wise-compile.el is end here
