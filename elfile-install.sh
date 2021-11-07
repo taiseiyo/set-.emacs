1c8340025ee86ddf368d2594b01ed38322a5303e
suzuki taisei <taisei@localhost>
2021-11-08 01:00:30 +0900

    fix

--- elfile-install.sh
+++ elfile-install.sh
@@ -7,3 +7,9 @@ emacsclient -e "(package-refresh-contents)"
 for pkg in $package_list;do
     emacsclient -e "(package-install '$pkg)"
 done
+
+for dotfile in $(ls dotfiles | grep set | sed -nr "s/set-(.*)/\1/p");do
+    cp $dotfile $HOME/
+done
+
+sudo cp el-files/* /usr/share/emacs/27.1/site-lisp/
