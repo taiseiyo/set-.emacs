eaf9164f686ab7c4440997c84242bcc1d0da7c12
suzuki taisei <taisei@localhost>
2021-12-20 12:15:46 +0900

    add fzf

--- elfile-install.sh
+++ elfile-install.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+# skkdic-extra → 日本語の追加登録
 sudo apt install skkdic-extra
 
 package_list="helm jedi tern tern-auto-complete emmet-mode prettier-js undo-tree lua-mode codic rjsx-mode flycheck irony meghanada tide clang-format visual-regexp-steroids"
@@ -14,4 +15,5 @@ for dotfile in $(ls dotfiles | grep set | sed -nr "s/set-(.*)/\1/p");do
     cp $dotfile $HOME/
 done
 
-sudo cp el-files/* /usr/share/emacs/27.1/site-lisp/
+# sudo cp el-files/* /usr/share/emacs/27.1/site-lisp/	       
+sudo cp el-files/* /usr/local/share/emacs/29.0.50/site-lisp/
