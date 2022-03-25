6977c0667bbc6d9fe70b11f34c768bafd43527e2
suzuki taisei <taisei@hawk>
2022-03-25 23:11:05 +0900

    fix

--- elfile-install.sh
+++ elfile-install.sh
@@ -1,20 +1,21 @@
 #!/usr/bin/env bash
 
 # skkdic-extra → 日本語の追加登録
-sudo apt install skkdic-extra
+sudo apt install skkdic-extra cmigemo fzf
 
 package_list="helm jedi tern tern-auto-complete emmet-mode prettier-js undo-tree lua-mode codic rjsx-mode flycheck irony meghanada tide clang-format visual-regexp-steroids ddskk"
 
 emacsclient -e "(package-refresh-contents)"
 
-for pkg in $package_list;do
+for pkg in $package_list ; do
+    echo $pkg 
     emacsclient -e "(package-install '$pkg)"
 done
 
-for dotfile in $(ls dotfiles | grep set | sed -nr "s/set-(.*)/\1/p");do
-    cp $dotfile $HOME/
+for dotfile in $(ls dotfiles | grep set | sed -nr "s/set-(.*)/\1/p") ; do
+    cp dotfiles/$dotfile $HOME/
 done
 
-# sudo cp el-files/* /usr/share/emacs/27.1/site-lisp/	       
-sudo cp el-files/* /usr/local/share/emacs/29.0.50/site-lisp/
+
+sudo cp el-files/* /usr/local/share/emacs/28.0.92/site-lisp/
 cp el-files/init.el el-files/early-init.el ~/.emacs.d/
